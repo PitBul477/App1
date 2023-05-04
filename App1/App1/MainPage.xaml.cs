9787dc7414441fb1fe7ba9f0100f35e5b6c95b68
PitBul477 <45077635+PitBul477@users.noreply.github.com>
2023-05-04 15:42:26 +0600

    Initial commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _nickname;
+        private string _ipAddress = "92.124.142.200";
+        private int _port = 12345;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            SendPing(1);
+            SendPing(2);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+            if (Entry.Text.Length > 0)
+            {
+                _nickname = Entry.Text;
+                Button2.IsEnabled = true;
+                Entry.IsEnabled = false;
+                Label.IsVisible = false;
+                Button.Focus();
+            }
+            else
+                DisplayAlert("Ошибка", "НИК НЕ ВВЕДЁН", "OK");
+        }
+
+        catch (Exception ex)
+            {
+                Label.Text = $"Error: {ex.Message}";
+                Label.IsVisible = true;
+            }
+        }
+
+        private void SendPing(int pingNumber)
+        {
+            try
+            {
+                //IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                IPAddress ipAddress = IPAddress.Parse(_ipAddress);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(new IPEndPoint(ipAddress, _port));
+                string currentTime = DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss:fff");
+                string message = $"{pingNumber}|{currentTime}|{_nickname}";
+                byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+                socket.Send(messageBytes);
+                byte[] buffer = new byte[1024];
+                int bytes = socket.Receive(buffer);
+                string response = System.Text.Encoding.UTF8.GetString(buffer, 0, bytes);
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                Label.Text = response;
+            }
+            catch (Exception ex)
+            {
+                Label.Text = $"Error: {ex.Message}";
+                Label.IsVisible = true;
+            }
+        }
+        private void EyeButton_Clicked(object sender, EventArgs e)
+        {
+            Label.IsVisible = !Label.IsVisible;
+        }
+
+        private async void SettingsButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SettingsPage(this));
+        }
+        
+        public void SetIP(string ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+    }
+}
---
+++ App1/App1/SettingsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        private MainPage _form1;
+        public SettingsPage(MainPage form1)
+        {
+            InitializeComponent();
+            _form1 = form1;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _form1.SetIP(IP.Text, ushort.Parse(Ports.Text));
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            _form1.SetIP("92.124.142.200", 12345);
+            IP.Text = "";
+            Ports.Text = "";
+        }
+    }
+}
